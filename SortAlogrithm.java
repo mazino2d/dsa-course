80e788c9931f7eeac4d6ca543973cf36e652b32a
urekmaizotog <khoi.do.hpc@outlook.com>
2019-12-13 13:58:22 +0700

    optimized bubble sort

--- SortAlogrithm.java
+++ SortAlogrithm.java
@@ -12,11 +12,10 @@ class SortAlgorithm {
 
     // https://www.geeksforgeeks.org/selection-sort/
     public static <E extends Comparable<E>> void selection_sort(E[] array) {
-        int size = array.length;
         // One by one move boundary of unsorted subarray 
-        for(int i = 0; i < size; ++i) {
+        for(int i = 0; i < array.length; ++i) {
             int idx = i; // Find the minimum element in unsorted array
-            for(int j = i + 1; j < size; ++j)
+            for(int j = i + 1; j < array.length; ++j)
                 if(array[idx].compareTo(array[j]) > 0) idx = j;
             // Swap the found minimum element with the first element
             swap(array, i, idx);
@@ -25,17 +24,34 @@ class SortAlgorithm {
 
     // https://www.geeksforgeeks.org/bubble-sort/
     public static <E extends Comparable<E>> void naive_bubble_sort(E[] array) {
-        int size = array.length;
-        
-        for(int i = 0; i < size - 1; ++i) {
-            for(int j = size - 1; j > i; --j)
-                if(array[j].compareTo(array[j - 1]) < 0) swap(array, j, j - 1);
+        for(int i = 0; i < array.length - 1; ++i)
+            for(int j = array.length - 1; j > i; --j)
+                 // swap the adjacent elements if they are in wrong order.
+                if(array[j].compareTo(array[j - 1]) < 0)  swap(array, j, j - 1);
+    }
+
+    public static <E extends Comparable<E>> void optimized_bubble_sort(E[] array) {
+        for(int i = 0; i < array.length - 1; ++i) {
+            boolean swapped = false;
+            for(int j = array.length - 1; j > i; --j)
+                 // swap the adjacent elements if they are in wrong order.
+                if(array[j].compareTo(array[j - 1]) < 0) 
+                    {swap(array, j, j - 1); swapped = true;}
+            if(!swapped) return;
         }
     }
 
+    // // https://www.geeksforgeeks.org/cocktail-sort/
+    // public static <E extends Comparable<E>> void cocktail_sort(E[] array) {
+        
+    // }
+
+    
     public static void main(String[] args) {
         Integer[] array = new Integer[] {9, 1, 3, 5, 2, 6,  4, 8, 7, 0};
-        naive_bubble_sort(array); print(array);
-
+        // selection_sort(array); print(array);
+        // naive_bubble_sort(array); print(array);
+        optimized_bubble_sort(array); print(array);
+        // cocktail_sort(array); print(array);
     }
 }
